19dc1fb73243bbac0a3799cf18f82f94039a0b49
Ashur Cabrera <hello@ashur.cab>
2022-10-02 17:08:49 -0600

    🔨 Move classnames to standalone function
    This allows other filters or shortcodes to use the same functionality

--- src/shortcodes/classnames.js
+++ src/shortcodes/classnames.js
@@ -1,27 +1,6 @@
+const classnames = require( "../lib/classnames" );
+
 /**
  * @returns {Function}
  */
-module.exports.shortcode = () =>
-{
-	/**
-	 * Join truthy, non-duplicate argument values into a space-delimited string.
-	 *
-	 * @example
-	 * {% classnames
-	 *   "block",
-	 *   "block__element",
-	 *   "block__element--modifier" if false,
-	 *   "block"
-	 * %}
-	 * // returns "block block__element"
-	 * @param {string[]} args
-	 * @return {string}
-	 */
-	return ( ...args ) =>
-	{
-		return args
-			.filter( ( arg, index ) => args.indexOf( arg ) === index ) // ensure values are unique
-			.filter( ( arg ) => arg ) // only include truthy values
-			.join( " " );
-	};
-};
+module.exports.shortcode = () => classnames;
