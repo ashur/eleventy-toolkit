cd45d383c6346c13ff6db830e27e80f7e0464ddf
Ashur Cabrera <hello@ashur.cab>
2022-10-02 19:29:00 -0600

    :hammer: Move styles to standalone function
    This allows other filters or shortcodes to use the same functionality

--- src/shortcodes/styles.js
+++ src/shortcodes/styles.js
@@ -1,36 +1,6 @@
+const styles = require( "../lib/styles" );
+
 /**
  * @returns {Function}
  */
-module.exports.shortcode = () =>
-{
-	/**
-	 * Join truthy object values into a semicolon-delimited string.
-	 *
-	 * @example
-	 * {% styles {
-	 *     "background-color": backgroundColor,
-	 *     "--custom-property": customProperty,
-	 *     "--undefined-property": "green" if undefinedProperty
-	 * } %}
-	 * // returns "background-color: red; --custom-property: 10px"
-	 * @param {Object} args
-	 * @return {string}
-	 */
-	return ( args ) =>
-	{
-		if( !args )
-		{
-			return "";
-		}
-
-		return Object.entries( args )
-			/* eslint-disable-next-line no-unused-vars */
-			.filter( ( [property, value] ) => (
-				value !== false &&
-				value !== null &&
-				value !== undefined
-			) ) // only include truthy values
-			.map( ( [property, value] ) => `${property}: ${value}` )
-			.join( "; " );
-	};
-};
+module.exports.shortcode = () => styles;
