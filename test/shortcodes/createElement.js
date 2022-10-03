bf067ffba732137c91c49e41d87e144d5518db05
Ashur Cabrera <hello@ashur.cab>
2022-10-02 19:26:37 -0600

    :white_check_mark: Reorganize createElement tests

--- test/shortcodes/createElement.js
+++ test/shortcodes/createElement.js
@@ -4,120 +4,132 @@ const { assert } = require( "chai" );
 
 describe( "createElement (paired shortcode)", () =>
 {
-	it( "should return an HTML element using the tagName provided", () =>
+	describe( "#innerHTML", () =>
 	{
-		assert.equal( createElement( undefined, "a" ), "<a></a>" );
+		it( "should return an HTML element using the innerHTML provided", () =>
+		{
+			assert.equal(
+				createElement(
+					"<h1>Hello, world</h1>",
+					"article",
+					{
+						class: "post post--new",
+						"data-example": "Lorem ipsum",
+					},
+				),
+				"<article class=\"post post--new\" data-example=\"Lorem ipsum\"><h1>Hello, world</h1></article>",
+			);
+		} );
 	} );
 
-	it( "should return an HTML element using the attributes provided", () =>
+	describe( "#tagName", () =>
 	{
-		assert.equal(
-			createElement(
-				undefined,
-				"article",
-				{
-					class: "post post--new",
-					"data-example": "Lorem ipsum",
-				},
-			),
-			"<article class=\"post post--new\" data-example=\"Lorem ipsum\"></article>",
-		);
-	} );
+		it( "should return an HTML element using the tagName provided", () =>
+		{
+			assert.equal( createElement( undefined, "a" ), "<a></a>" );
+		} );
 
-	it( "should omit attributes whose values are undefined or null", () =>
-	{
-		assert.equal(
-			createElement(
-				undefined,
-				"article",
-				{
-					class: "card",
-					href: undefined,
-					"data-example": null,
-					"aria-hidden": false,
-				},
-			),
-			"<article class=\"card\" aria-hidden=\"false\"></article>",
-		);
-	} );
-
-	it( "should return a self-closing HTML element if specified", () =>
-	{
-		assert.equal(
-			createElement(
-				undefined,
-				"source",
-				{
-					width: 500,
-				},
-				true,
-			),
-			"<source width=\"500\">",
-		);
-	} );
-
-	it( "should ignore innerHTML if self-closing HTML element is specified", () =>
-	{
-		assert.equal(
-			createElement(
+		it( "should return innerHTML only if tagName is falsy", () =>
+		{
+			assert.equal(
+				createElement(
+					"hello, world",
+					undefined,
+					{
+						width: 500,
+					},
+					true,
+				),
 				"hello, world",
-				"source",
-				{
-					width: 500,
-				},
-				true,
-			),
-			"<source width=\"500\">",
-		);
+			);
+		} );
 	} );
 
-	it( "should return innerHTML only if tagName is falsy", () =>
+	describe( "#attributes", () =>
 	{
-		assert.equal(
-			createElement(
-				"hello, world",
-				undefined,
-				{
-					width: 500,
-				},
-				true,
-			),
-			"hello, world",
-		);
+		it( "should return an HTML element using the attributes provided", () =>
+		{
+			assert.equal(
+				createElement(
+					undefined,
+					"article",
+					{
+						class: "post post--new",
+						"data-example": "Lorem ipsum",
+					},
+				),
+				"<article class=\"post post--new\" data-example=\"Lorem ipsum\"></article>",
+			);
+		} );
+
+		it( "should omit attributes whose values are undefined or null", () =>
+		{
+			assert.equal(
+				createElement(
+					undefined,
+					"article",
+					{
+						class: "card",
+						href: undefined,
+						"data-example": null,
+						"aria-hidden": false,
+					},
+				),
+				"<article class=\"card\" aria-hidden=\"false\"></article>",
+			);
+		} );
+
+		it( "should use `classnames` if `class` attribute value is an array", () =>
+		{
+			assert.equal(
+				createElement(
+					undefined,
+					"div",
+					{
+						class: [
+							"block",
+							undefined,
+							"block__element",
+							false,
+							"block__element--modifier",
+						],
+					},
+				),
+				"<div class=\"block block__element block__element--modifier\"></div>",
+			);
+		} );
 	} );
 
-	it( "should return an HTML element using the innerHTML provided", () =>
+	describe( "#selfClosing", () =>
 	{
-		assert.equal(
-			createElement(
-				"<h1>Hello, world</h1>",
-				"article",
-				{
-					class: "post post--new",
-					"data-example": "Lorem ipsum",
-				},
-			),
-			"<article class=\"post post--new\" data-example=\"Lorem ipsum\"><h1>Hello, world</h1></article>",
-		);
-	} );
+		it( "should return a self-closing HTML element if specified", () =>
+		{
+			assert.equal(
+				createElement(
+					undefined,
+					"source",
+					{
+						width: 500,
+					},
+					true,
+				),
+				"<source width=\"500\">",
+			);
+		} );
 
-	it( "should use `classnames` if `class` attribute value is an array", () =>
-	{
-		assert.equal(
-			createElement(
-				undefined,
-				"div",
-				{
-					class: [
-						"block",
-						undefined,
-						"block__element",
-						false,
-						"block__element--modifier",
-					],
-				},
-			),
-			"<div class=\"block block__element block__element--modifier\"></div>",
-		);
+		it( "should ignore innerHTML if self-closing HTML element is specified", () =>
+		{
+			assert.equal(
+				createElement(
+					"hello, world",
+					"source",
+					{
+						width: 500,
+					},
+					true,
+				),
+				"<source width=\"500\">",
+			);
+		} );
 	} );
 } );
